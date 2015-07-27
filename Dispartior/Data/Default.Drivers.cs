585735ecc10e66f0c562cb6a2ddb2420579db89b
AnObfuscator <anobfuscator@gmail.com>
2015-07-27 13:31:30 -0500

    Implement support for default drivers.

--- Dispartior/Data/Default.Drivers.cs
+++ Dispartior/Data/Default.Drivers.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using Dispartior.Data.Drivers;
 
 namespace Dispartior.Data
 {
@@ -6,16 +9,20 @@ namespace Dispartior.Data
     {
         public static class Driver
         {
+            private static IDictionary<string, Type> drivers = new Dictionary<string, Type>
+            {
+                { typeof(DispartiorCacheDriver).FullName, typeof(DispartiorCacheDriver) }
+            };
+
             public static bool ExistsFor(string driverName)
             {
-                throw new NotImplementedException();
+                return drivers.ContainsKey(driverName);
             }
 
             public static Type Named(string driverName)
             {
-                throw new NotImplementedException();
+                return drivers[driverName];
             }
         }
     }
 }
-
